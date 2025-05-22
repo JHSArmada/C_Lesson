5fa911a7c4a99778bf97f23d46decb676bf74ca6
VArMaDa <gojhs82@naver.com>
2025-05-22 19:29:44 +0900

    테스트

---
+++ Lesson06/Lesson06.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+git config --global user.name "VArMaDa"
+git config --global user.email "gojhs82@naver.com"
+
+git clone https://github.com/JHSArmada/C_Lesson.git
+
+복습을 한다
+저장 하고....
+
+git add.
+git commit -m
+git push origin main
+
+//학원에서
+git pull origin main
+
+*/
+
+
+
+
+
+
+int main()
+{
+	return 0;
+}
---
+++ lesson06.cpp
@@ -0,0 +1,30 @@
+[33mcommit fe04a46d8a9ad74d2ba5521d6bd8d02520db21be[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Merge: 7e640ab 1d6e2a9
+Author: VArMaDa <gojhs82@naver.com>
+Date:   Tue May 20 21:52:21 2025 +0900
+
+    Resolved merge conflict
+
+[33mcommit 7e640ab5bbb8eeee97e73695eee970b17b280fd1[m
+Author: VArMaDa <gojhs82@naver.com>
+Date:   Tue May 20 21:48:46 2025 +0900
+
+    비트연산자까지 완료
+
+[33mcommit 67eb3fc6350acfe877f789578dd4b21181163311[m
+Author: VArMaDa <gojhs82@naver.com>
+Date:   Tue May 20 20:57:57 2025 +0900
+
+    OPERATORS
+
+[33mcommit 1d6e2a9a98b5f5949a9aaa7d2f66ff64308da830[m
+Author: JHSArmada <gojhs82@naver.com>
+Date:   Tue May 20 20:54:11 2025 +0900
+
+    Initial commit
+
+[33mcommit d502ca6693228306a64709b5f6420a61384726e8[m
+Author: VArMaDa <gojhs82@naver.com>
+Date:   Tue May 20 20:40:52 2025 +0900
+
+    연산자까지 완료
